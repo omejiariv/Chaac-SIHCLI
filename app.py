69ec4df0ef94415707c48da7cac4bbf786e2bd09
omejiariv <145706652+omejiariv@users.noreply.github.com>
2025-09-17 10:17:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -738,7 +738,7 @@ def main():
             
             with tab_tendencias:
                 display_trends_forecast_tab(df_monthly_filtered, stations_for_analysis)
-        
+            
             else:
         st.warning("Por favor, suba todos los archivos requeridos en la barra lateral para comenzar.")
         display_welcome_tab()
