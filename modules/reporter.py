3bbd1a8a0ba56ddc90f8eb29c38ca889f252a8d4
omejiariv <145706652+omejiariv@users.noreply.github.com>
2025-10-03 20:46:26 -0500

    Create reporter.py

---
+++ modules/reporter.py
@@ -0,0 +1,12 @@
+# reporter.py
+
+def generate_pdf_report(
+    report_title, 
+    sections_to_include, 
+    gdf_filtered, 
+    df_anual_melted,
+    df_monthly_filtered,
+    # ... otros dataframes y datos necesarios
+):
+    # Lógica para construir el PDF
+    pass
