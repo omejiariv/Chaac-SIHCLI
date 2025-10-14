3bb6358e079fca29c6506d66c91a0676948207ce
omejiariv <145706652+omejiariv@users.noreply.github.com>
2025-10-14 00:23:56 -0500

    Update interpolation.py

--- modules/interpolation.py
+++ modules/interpolation.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import geopandas as gpd
 import numpy as np
-import gstools as gs
-from gstools import Idw
+# import gstools as gs
+# from gstools import Idw
 import rasterio
 from rasterio.transform import from_origin
 from rasterio.mask import mask
