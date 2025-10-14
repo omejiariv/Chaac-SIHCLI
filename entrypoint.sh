021e0c8931d160b407bcd9c8af5e56f1b4da5d38
omejiariv <omejiariv@gmail.com>
2025-10-13 22:28:17 -0500

    Final coordinated fix for all files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-
-# Aumenta el límite de inotify para la monitorización de archivos
 echo fs.inotify.max_user_watches=524288 | tee -a /etc/sysctl.conf && sysctl -p
-
-# Inicia la aplicación de Streamlit
 exec streamlit run app.py
