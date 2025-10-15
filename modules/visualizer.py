13581e161f29f2cc7b2d655d168fb623f5da1c09
omejiariv <145706652+omejiariv@users.noreply.github.com>
2025-10-15 02:59:54 -0500

    Update visualizer.py

--- modules/visualizer.py
+++ modules/visualizer.py
@@ -1577,6 +1577,8 @@ def display_advanced_maps_tab(gdf_filtered, stations_for_analysis, df_anual_melt
                 st.warning("Los datos de cuencas no están disponibles para este modo.")
         
         else: # MODO REGIONAL
+
+            
             df_anual_non_na = df_anual_melted.dropna(subset=[Config.PRECIPITATION_COL])
             if not stations_for_analysis or df_anual_non_na.empty:
                 st.warning("No hay suficientes datos anuales para realizar la interpolación.")
